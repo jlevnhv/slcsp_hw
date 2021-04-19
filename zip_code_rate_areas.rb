d9d49077c2de0f92bc8a5e7ef4307bf415fd88be
Josh Levinson <joshua.levinson@gmail.com>
2021-04-19 14:11:58 -0400

    Bug in duplicate plan areas for zip

--- zip_code_rate_areas.rb
+++ zip_code_rate_areas.rb
@@ -13,17 +13,21 @@ class ZipCodeRateAreas
         @zip_code_rate_areas[zip_code] = []
       end
 
-      @zip_code_rate_areas[zip_code] << rate_area
+      # add rate area unless we've already seen it
+      unless @zip_code_rate_areas[zip_code].include?(rate_area)
+        @zip_code_rate_areas[zip_code] << rate_area
+      end
     end
 
     # remove any zip codes which have more than one rate area
-    # since these are ambiguous, then just take the plan value
+    # since these are ambiguous
     @zip_code_rate_areas.select! do |zip_code, rate_areas|
       rate_areas.length == 1
     end
 
-    # pop each value so we have an actual value and not an array
+    # pop each value in hash so we have an actual value and not an array
     # i.e. instead of { '12345': ['1']} we get { '12345': '1' }
+    # there's probably a better way to do this
     @zip_code_rate_areas.each_with_object({}) do |(key, value), new_hash|
       new_hash[key] = value.pop
     end
